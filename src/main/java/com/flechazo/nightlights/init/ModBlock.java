7d60442901f6775b67e849d7a47690f4659729c1
Flechazo <2558755403@qq.com>
2025-11-23 13:34:18 +0800

    1.21.9-10

--- src/main/java/com/flechazo/nightlights/init/ModBlock.java
+++ src/main/java/com/flechazo/nightlights/init/ModBlock.java
@@ -152,7 +152,7 @@ public class ModBlock {
                 BlockBehaviour.Properties.ofFullCopy(Blocks.FLOWER_POT)
                         .noOcclusion()
                         .sound(SoundType.GLASS)
-                        .noCollission()
+                        .noCollision()
                         .instabreak()
                         .setId(blockKey)
         );
